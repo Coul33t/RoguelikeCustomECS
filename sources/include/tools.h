b64f5431c7338f1cf020289b24b424c233a8d4ac
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2023-10-16 15:26:24 +0200

    :sparkles: Added debugging defines

---
+++ sources/include/tools.h
@@ -0,0 +1,13 @@
+//
+// Created by Kontin on 16/10/2023.
+//
+
+#ifndef ROGUELIKECUSTOMECS_TOOLS_H
+#define ROGUELIKECUSTOMECS_TOOLS_H
+
+#define FULL_LOCATION "[" << __FILE__ << " | " << __FUNCTION__ << " | l." << __LINE__ << "]"
+#define FILE_NAME "[" << __FILE__ << "]"
+#define FUNCTION_NAME "[" << __FUNCTION__ << "]"
+#define LINE "l." << __LINE__
+
+#endif //ROGUELIKECUSTOMECS_TOOLS_H
