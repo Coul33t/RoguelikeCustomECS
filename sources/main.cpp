c60f384a574108944f3ed7cde20fc7856a163b40
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2023-10-16 15:27:05 +0200

    :construction: Refactoring

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <any>
 
-#include "include/entities/entity.h"
 #include "include/engine.h"
-#include "include/systems/system_move.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
